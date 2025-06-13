57ec7dcea6ebdef2f3d1f0bebc5994741e469896
erezul77 <erezsnz@gmail.com>
2025-06-13 12:44:50 +0300

    Clean Vercel deployable version

--- lib/blockchain/pushToChain.ts
+++ lib/blockchain/pushToChain.ts
@@ -1,15 +1,12 @@
-import { JsonRpcProvider, Wallet, Contract } from 'ethers'
-import contractABI from './abi/ReflectionVaultABI.json'
+import { JsonRpcProvider } from 'ethers/providers';
+import { Wallet, Contract } from 'ethers';
+import contractABI from './contractABI.json';
 
-const contractAddress = process.env.REFLECTION_VAULT_CONTRACT!
+const contractAddress = process.env.CONTRACT_ADDRESS!;
 
 export async function pushToChain(ipfsHash: string, adequacy: number) {
-  const provider = new JsonRpcProvider(process.env.ALCHEMY_RPC_URL!)
-  const wallet = new Wallet(process.env.PRIVATE_KEY!, provider)
-  const contract = new Contract(contractAddress, contractABI, wallet)
-
-  const tx = await contract.submitReflection(ipfsHash, adequacy)
-  console.log('TX submitted:', tx.hash)
-  await tx.wait()
-  console.log('TX confirmed')
+  const provider = new JsonRpcProvider(process.env.ALCHEMY_RPC_URL);
+  const wallet = new Wallet(process.env.PRIVATE_KEY!, provider);
+  const contract = new Contract(contractAddress, contractABI, wallet);
+  await contract.submitReflection(ipfsHash, adequacy);
 }
--- lib/ipfs.ts
+++ lib/ipfs.ts
@@ -1,16 +1,9 @@
-import * as w3 from '@web3-storage/w3up-client'
-import { filesFromPaths } from 'files-from-path'
+import { createUploader } from '@web3-storage/w3up-client';
 
-const spaceDID = process.env.WEB3_STORAGE_SPACE_DID!
-const privateKey = process.env.WEB3_STORAGE_PRIVATE_KEY!
-const email = process.env.WEB3_STORAGE_EMAIL!
+const client = await createUploader();
 
-const client = await w3.create()
-await client.login(email)
-await client.setCurrentSpace(spaceDID)
-
-export async function pinToIPFS(filePath: string): Promise<string> {
-  const files = await filesFromPaths([filePath])
-  const cid = await client.uploadFile(files[0])
-  return cid.toString()
+export async function uploadToIPFS(content: string) {
+  const blob = new Blob([content], { type: 'text/plain' });
+  const cid = await client.uploadFile(blob);
+  return cid.toString();
 }
